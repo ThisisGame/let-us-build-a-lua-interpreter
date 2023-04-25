bcd7eadb401de526220258961e8300924e689f55
陈鹏 <980097191@qq.com>
2023-04-25 17:08:00 +0000

    comment

--- C02/dummylua-2-3/common/luastring.c
+++ C02/dummylua-2-3/common/luastring.c
@@ -95,15 +95,28 @@ int luaS_resize(struct lua_State* L, unsigned int nsize) {
 }
 
 
+/**
+ * @brief 这个函数用于创建一个新的字符串对象。
+ *
+ * @param L lua_State 状态机指针
+ * @param str const char* 字符串起始地址
+ * @param tag int 字符串类型（LUA_SHRSTR或LUA_LNGSTR）
+ * @param l unsigned int 字符串长度
+ * @param hash unsigned int 字符串hash值
+ * 
+ * @return struct TString* 返回一个新的TString对象
+ */
 static struct TString* createstrobj(struct lua_State* L, const char* str, int tag, unsigned int l, unsigned int hash) {
     size_t total_size = sizelstring(l);
 
+    // 创建新的对象并填充内存
     struct GCObject* o = luaC_newobj(L, tag, total_size);
     struct TString* ts = gco2ts(o);
     memcpy(getstr(ts), str, l * sizeof(char));
     getstr(ts)[l] = '\0';
     ts->extra = 0;
 
+    // 填充不同类型的字段
     if (tag == LUA_SHRSTR) {
         ts->shrlen = cast(lu_byte, l);
         ts->hash = hash;
@@ -120,15 +133,28 @@ static struct TString* createstrobj(struct lua_State* L, const char* str, int ta
     return ts;
 }
 
+
 // only short strings can be interal
+/**
+ * @brief 这个函数用于创建一个内部字符串。
+ *
+ * @param L lua_State 状态机指针
+ * @param str const char* 字符串起始地址
+ * @param l unsigned int 字符串长度
+ * 
+ * @return struct TString* 返回一个新的TString对象
+ */
 static struct TString* internalstr(struct lua_State* L, const char* str, unsigned int l) {
+    // 获取全局状态和字符串表
     struct global_State* g = G(L);
     struct stringtable* tb = &g->strt;
+    // 计算字符串hash值，从字符串表中查找该字符串是否已经存在
     unsigned int h = luaS_hash(L, str, l, g->seed); 
     struct TString** list = &tb->hash[lmod(h, tb->size)];
 
     for (struct TString* ts = *list; ts; ts = ts->u.hnext) {
         if (ts->shrlen == l && (memcmp(getstr(ts), str, l * sizeof(char)) == 0)) {
+            // 如果该字符串已经存在，并且已经被回收，则重新标记为白色
             if (isdead(g, ts)) {
                 changewhite(ts);
             }
@@ -136,6 +162,7 @@ static struct TString* internalstr(struct lua_State* L, const char* str, unsigne
         }
     }
 
+    // 如果该字符串不存在，则创建一个新的TString对象，并插入字符串表中
     if (tb->nuse >= tb->size && tb->size < INT_MAX / 2) {
         luaS_resize(L, tb->size * 2);
         list = &tb->hash[lmod(h, tb->size)];
@@ -149,49 +176,101 @@ static struct TString* internalstr(struct lua_State* L, const char* str, unsigne
     return ts;
 }
 
+
+/**
+ * @brief 创建一个新的字符串，并返回TString对象指针。
+ *
+ * @param L lua_State 状态机指针
+ * @param str const char* 字符串起始地址
+ * @param l unsigned int 字符串长度
+ * 
+ * @return struct TString* 返回一个新的TString对象
+ */
 struct TString* luaS_newlstr(struct lua_State* L, const char* str, unsigned int l) {
+    // 如果字符串长度小于等于MAXSHORTSTR，则调用internalstr函数创建短字符串
     if (l <= MAXSHORTSTR) {
         return internalstr(L, str, l); 
     }
     else {
+        // 否则调用luaS_createlongstr函数创建长字符串
         return luaS_createlongstr(L, str, l); 
     }
 }
 
+
+/**
+ * @brief 创建一个新的字符串，并返回TString对象指针。如果该字符串已存在，则直接返回缓存中的TString对象指针。
+ *
+ * @param L lua_State 状态机指针
+ * @param str const char* 字符串起始地址
+ * @param l unsigned int 字符串长度
+ * 
+ * @return struct TString* 返回一个新的TString对象，或者缓存中已有的TString对象。
+ */
 struct TString* luaS_new(struct lua_State* L, const char* str, unsigned int l) {
+    // 计算字符串的哈希值
     unsigned int hash = point2uint(str); 
+    // 根据哈希值定位在缓存数组中的位置
     int i = hash % STRCACHE_M;
+    // 遍历指定位置的缓存链表
     for (int j = 0; j < STRCACHE_N; j ++) {
+        // 获取当前遍历到的TString对象指针
         struct TString* ts = G(L)->strcache[i][j];
+        // 判断该TString对象指针是否与要创建的字符串相同
         if (strcmp(getstr(ts), str) == 0) {
+            // 如果相同，直接返回该TString对象指针
             return ts;
         }
     }
 
+    // 将缓存数组中除了第一个元素以外的元素往后移动一位
     for (int j = STRCACHE_N - 1; j > 0; j--) {
         G(L)->strcache[i][j] = G(L)->strcache[i][j - 1];
     }
 
+    // 创建一个新的TString对象，并存储在缓存数组的第一个位置
     G(L)->strcache[i][0] = luaS_newlstr(L, str, l);
+    // 返回该TString对象指针
     return G(L)->strcache[i][0];
 }
 
-// remove TString from stringtable, only for short string
+
+
+/**
+ * @brief 从全局字符串表中移除一个指定的TString对象指针。
+ *
+ * @param L lua_State 状态机指针
+ * @param ts struct TString* 要移除的TString对象指针
+ */
 void luaS_remove(struct lua_State* L, struct TString* ts) {
+    // 获取全局状态变量指针
     struct global_State* g = G(L);
+    // 计算要移除的TString对象应该所在的哈希桶位置，并获取该桶位置的链表指针
     struct TString** list = &g->strt.hash[lmod(ts->hash, g->strt.size)];
 
+    // 遍历该链表
     for (struct TString* o = *list; o; o = o->u.hnext) {
-        if (o == ts) {
+        if (o == ts) { // 如果找到了要移除的TString对象
+            // 将当前链表元素指针指向下一个元素的指针，相当于删除当前链表元素
             *list = o->u.hnext;
             break;
         }
+        // 更新当前元素的指针，使其指向下一个元素的指针
         list = &(*list)->u.hnext;
     }
 }
 
+
+/**
+ * @brief 清空Lua字符串缓存中的white字符串。
+ *
+ * @param L lua_State 状态机指针
+ */
 void luaS_clearcache(struct lua_State* L) {
+    // 获取全局状态变量指针
     struct global_State* g = G(L);
+
+    // 遍历Lua字符串缓存，将white字符串替换为错误消息字符串
     for (int i = 0; i < STRCACHE_M; i ++) {
         for (int j = 0; j < STRCACHE_N; j ++) {
             if (iswhite(g->strcache[i][j])) {
@@ -201,31 +280,110 @@ void luaS_clearcache(struct lua_State* L) {
     }
 }
 
+
+/**
+ * @brief 判断两个短字符串是否相等。
+ *
+ * 如果 a 和 b 指向同一TString对象，则认为它们相等；
+ * 如果 a 和 b 两个TString对象的长度和内容都相同，则认为它们相等。
+ *
+ * @param L lua_State 状态机指针
+ * @param a 待比较的第一个 TStirng 对象指针
+ * @param b 待比较的第二个 TStirng 对象指针
+ * @return int 相等返回 1，不等返回 0
+ */
 int luaS_eqshrstr(struct lua_State* L, struct TString* a, struct TString* b) {
-    return (a == b) || (a->shrlen == b->shrlen && strcmp(getstr(a), getstr(b)) == 0);
+    // 如果 a 和 b 指向同一TString对象，则认为它们相等
+    return (a == b) ||
+        // 如果 a 和 b 两个TString对象的长度和内容都相同，则认为它们相等
+        (a->shrlen == b->shrlen && strcmp(getstr(a), getstr(b)) == 0);
 }
 
+
+/**
+ * @brief 判断两个长字符串是否相等。
+ *
+ * 如果 a 和 b 指向同一TString对象，则认为它们相等；
+ * 如果 a 和 b 两个TString对象的长度和内容都相同，则认为它们相等。
+ *
+ * @param L lua_State 状态机指针
+ * @param a 待比较的第一个 TStirng 对象指针
+ * @param b 待比较的第二个 TStirng 对象指针
+ * @return int 相等返回 1，不等返回 0
+ */
 int luaS_eqlngstr(struct lua_State* L, struct TString* a, struct TString* b) {
-    return (a == b) || (a->u.lnglen == b->u.lnglen && strcmp(getstr(a), getstr(b)) == 0);
+    // 如果 a 和 b 指向同一TString对象，则认为它们相等
+    return (a == b) ||
+        // 如果 a 和 b 两个TString对象的长度和内容都相同，则认为它们相等
+        (a->u.lnglen == b->u.lnglen && strcmp(getstr(a), getstr(b)) == 0);
 }
 
+
+/**
+ * @brief 对传入的字符串进行哈希计算。
+ *
+ * 此函数采用 BKDR 算法。具体实现为对传入的 str 字符串中的每个第 step 个字符取出，
+ * 进行哈希计算，按位运算得到最终结果并返回。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待哈希的字符串指针
+ * @param l 字符串长度
+ * @param h 哈希值的初值（如果调用方未提供，则可以赋值为随机数，比如 5381）
+ * @return unsigned int 计算得到的哈希值
+ */
 unsigned int luaS_hash(struct lua_State* L, const char* str, unsigned int l, unsigned int h) {
+    // 按位异或上一次的哈希值和当前的字符串长度
     h = h ^ l;
+    // 设定步长
     unsigned int step = (l >> 5) + 1;
+    // 循环处理每一个步长的字符
     for (int i = 0; i < l; i = i + step) {
+        // 采用 BKDR 算法对字符进行哈希计算，并将结果与哈希值 h 做异或操作
         h ^= (h << 5) + (h >> 2) + cast(lu_byte, str[i]);
     }
+    // 返回计算得到的哈希值
     return h;
 }
 
+
+/**
+ * @brief 对传入的长字符串进行哈希计算。
+ *
+ * 此函数对于同一个字符串只会进行一次哈希计算，然后将结果缓存在 TString 结构
+ * 的 hash 字段中以供后续使用。如果调用方需要强制重新计算哈希值，则可以先将
+ * TString 结构的 extra 字段置为 0。
+ *
+ * @param L lua_State 状态机指针
+ * @param ts 待哈希的长字符串 TString 结构指针
+ * @return unsigned int 返回已缓存或新计算得到的哈希值
+ */
 unsigned int luaS_hashlongstr(struct lua_State* L, struct TString* ts) {
+    // 如果该 TString 结构尚未计算过哈希值，则进行计算
     if (ts->extra == 0) {
+        // 采用 luaS_hash 函数计算哈希值，并缓存该值和 extra 字段
         ts->hash = luaS_hash(L, getstr(ts), ts->u.lnglen, G(L)->seed);
         ts->extra = 1;
     }
+    // 返回 TString 结构保存的哈希值
     return ts->hash;
 }
 
+
+/**
+ * @brief 创建一个长字符串对象。
+ *
+ * 该函数接受一个 char* 类型的字符串和一个 size_t 类型的长度作为参数，并创建
+ * 一个 TString 类型的 Lua 对象。所谓长字符串，是指长度大于等于 LUAI_MAXSHORTLEN
+ * （即 40）的字符串。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待存储的字符串指针
+ * @param l 字符串的长度
+ * @return struct TString* 返回新创建的长字符串对象指针
+ */
 struct TString* luaS_createlongstr(struct lua_State* L, const char* str, size_t l) {
+    // 使用 createstrobj 函数创建一个 TString 类型的对象，并设置对象类型为 LUA_LNGSTR
+    // 并将传入的字符串与长度信息写入对象的 u.lng 字段中
     return createstrobj(L, str, LUA_LNGSTR, l, G(L)->seed);
 }
+
--- C02/dummylua-2-3/common/luastring.h
+++ C02/dummylua-2-3/common/luastring.h
@@ -24,21 +24,168 @@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #include "luastate.h"
 #include "../vm/luagc.h"
 
+/**
+ * @brief 计算长字符串对象大小。
+ *
+ * 该函数根据传入字符串长度计算一个长字符串对象所需的内存大小，用于后续对象分配
+ * 时调用。
+ *
+ * @param l 字符串长度
+ * @return size_t 返回长字符串对象所需的内存大小
+ */
 #define sizelstring(l) (sizeof(TString) + (l + 1) * sizeof(char))
+
+/**
+ * @brief 获取字符串数据。
+ *
+ * 该宏返回一个指针，指向传入 TString 对象（ts）中保存的字符串数据。具体实现中，
+ * 该宏返回了传入对象的 data 成员变量地址。
+ *
+ * @param ts TString 类型对象指针
+ * @return char* 返回字符串数据指针
+ */
 #define getstr(ts) (ts->data)
 
+/**
+ * @brief 初始化字符串模块。
+ *
+ * 该函数初始化字符串模块，并创建一个全局的字符串表用于存储 Lua 解释器中的所有字符
+ * 串。同时，该函数会为两个预定义的字符串（"nil" 和 "true"）在该表中分配空间并存
+ * 储。该函数应该在解释器初始化时被调用一次。
+ *
+ * @param L lua_State 状态机指针
+ * @return void
+ */
 void luaS_init(struct lua_State* L);
+
+/**
+ * @brief 调整全局字符串表大小。
+ *
+ * 该函数扩展或收缩全局字符串表的大小。如果新的表尺寸大于当前表的大小，则会分
+ * 配额外的空间以容纳更多的字符串。如果新表尺寸小于当前表的大小，则原有的字符
+ * 串对象可能会被回收。
+ *
+ * @param L lua_State 状态机指针
+ * @param nsize 新的表尺寸（单位为 TString 指针数）
+ * @return int 执行结果状态码，0 表示成功
+ */
 int luaS_resize(struct lua_State* L, unsigned int nsize); // only for short string
+
+/**
+ * @brief 创建一个新的短字符串对象。
+ * 
+ * 该函数接受一个 char* 类型的字符串和一个 unsigned int 类型的长度作为参数，并创建
+ * 一个 TString 类型的 Lua 对象。所谓短字符串，是指长度小于 LUAI_MAXSHORTLEN
+ * （即 40）的字符串。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待存储的字符串指针
+ * @param l 字符串的长度
+ * @return struct TString* 返回新创建的短字符串对象指针
+ */
 struct TString* luaS_newlstr(struct lua_State* L, const char* str, unsigned int l);
+
+/**
+ * @brief 创建一个新的字符串对象。
+ *
+ * 该函数根据传入字符串长度确定对象类型（短字符串或长字符串），并调用相应的函
+ * 数创建新的字符串对象。如果传入长度大于 LUAI_MAXSHORTLEN，则会创建一个长字符串对
+ * 象；否则将会创建一个短字符串对象。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待存储的字符串指针
+ * @param l 字符串的长度
+ * @return struct TString* 返回新创建的字符串对象指针
+ */
 struct TString* luaS_new(struct lua_State* L, const char* str, unsigned int l);
+
+/**
+ * @brief 从字符串表中移除指定的短字符串对象。
+ *
+ * 该函数从全局字符串表中移除传入的短字符串对象。注意，该函数仅用于短字符串对
+ * 象，且只能从全局字符串表中移除已存在的对象。
+ *
+ * @param L lua_State 状态机指针
+ * @param ts 待移除的短字符串对象指针
+ * @return void
+ */
 void luaS_remove(struct lua_State* L, struct TString* ts); // remove TString from stringtable, only for short string
 
+/**
+ * @brief 清空字符串缓存。
+ *
+ * 该函数会释放所有 Lua 解释器中保存的字符串对象，并重置全局字符串表大小为其初始
+ * 值。调用该函数后，Lua 执行环境中的所有字符串对象都将被回收。
+ *
+ * @param L lua_State 状态机指针
+ * @return void
+ */
 void luaS_clearcache(struct lua_State* L);
+
+/**
+ * @brief 判断两个短字符串是否相等。
+ *
+ * 该函数接受两个短字符串对象作为参数，并判断它们的值是否相等。
+ *
+ * @param L lua_State 状态机指针
+ * @param a 第一个短字符串对象指针
+ * @param b 第二个短字符串对象指针
+ * @return int 执行结果状态码，0 表示成功且两字符串相等
+ */
 int luaS_eqshrstr(struct lua_State* L, struct TString* a, struct TString* b);
+
+/**
+ * @brief 判断两个长字符串是否相等。
+ *
+ * 该函数接受两个长字符串对象作为参数，并判断它们的值是否相等。
+ *
+ * @param L lua_State 状态机指针
+ * @param a 第一个长字符串对象指针
+ * @param b 第二个长字符串对象指针
+ * @return int 执行结果状态码，0 表示成功且两字符串相等
+ */
 int luaS_eqlngstr(struct lua_State* L, struct TString* a, struct TString* b);
 
+/**
+ * @brief 计算字符串哈希值。
+ *
+ * 该函数计算传入字符串的哈希值，以便在全局字符串表中查找、插入或删除字符串。
+ * 该函数根据 Lua 设计文档中描述的字符串哈希算法实现，可保证正确性和一致性。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待计算哈希值的字符串指针
+ * @param l 字符串的长度
+ * @param h 上一个哈希值（用于解决冲突）
+ * @return unsigned int 返回计算得到的哈希值
+ */
 unsigned int luaS_hash(struct lua_State* L, const char* str, unsigned int l, unsigned int h);
+
+/**
+ * @brief 计算长字符串对象的哈希值。
+ *
+ * 该函数计算传入长字符串的哈希值，以便在全局字符串表中查找、插入或删除长字
+ * 符串。该函数采用加密哈希算法（MurmurHash2）实现，在平衡性和散列效率上均有不
+ * 错的表现。注意，该函数仅用于长字符串对象。
+ *
+ * @param L lua_State 状态机指针
+ * @param ts 待计算哈希值的长字符串对象指针
+ * @return unsigned int 返回计算得到的哈希值
+ */
 unsigned int luaS_hashlongstr(struct lua_State* L, struct TString* ts);
+
+/**
+ * @brief 创建一个长字符串对象。
+ *
+ * 该函数接受一个 char* 类型的字符串和一个 size_t 类型的长度作为参数，并创建一个
+ * TString 类型的 Lua 对象。所谓长字符串，是指长度大于等于 LUAI_MAXSHORTLEN
+ * （即 40）的字符串。
+ *
+ * @param L lua_State 状态机指针
+ * @param str 待存储的字符串指针
+ * @param l 字符串的长度
+ * @return struct TString* 返回新创建的长字符串对象指针
+ */
 struct TString* luaS_createlongstr(struct lua_State* L, const char* str, size_t l);
+
 
 #endif 
